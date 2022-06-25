c204d8e93604e90d490706ab44c7e92974cbb058
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2022-06-26 00:24:05 +0200

    add new action to check about the language

--- actions/actions.py
+++ actions/actions.py
@@ -4,142 +4,27 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 import re
-from actions.FCAI_DB import FCAI_DB
-from actions.English_to_Arabic_Mapping import map_subject_name
-
-# Access database
-db = FCAI_DB()
-""" if FCAI Subjects.dp is deleted uncomment below line """
-# db.create_DB()
-bylaw = db.get_tables('bylaw')
-gpa_dict = db.get_tables('GPA')
-schedule = db.get_schedule()
+from actions.schedule_handling import ask_for_one_subject, get_failed_subjects, gpa_dict, db
 
 
-def get_subjects_priority(student_schedule, gpa):
-    """
-        Parameters:
-            student_schedule: list of semester subject's
-            gpa: int of student's GPA
+class check_subject_name(Action):
+    def name(self) -> Text:
+        return "action_check_subject_name"
 
-        Return:
-            student_schedule: string of best schedule
-    """
-
-    min_priority = {}
-
-    for i in student_schedule:
-        if i not in bylaw.keys():
-            # if subject not in bylaw give it 0 priority
-            min_priority[i] = 0
-        else:
-            min_priority[i] = int(bylaw[i][-1])
-    # sort priorities ascending
-    min_priority = sorted(min_priority.items(), key=lambda x: x[1], reverse=False)
-
-    try:
-        """ check on gpa to know how many subjects can be registered by student """
-        if 1.5 < gpa < 2:
-            student_schedule.remove(min_priority[0][0])
-            student_schedule.remove(min_priority[1][0])
-
-        elif gpa < 1.5:
-            student_schedule.remove(min_priority[0][0])
-            student_schedule.remove(min_priority[1][0])
-            student_schedule.remove(min_priority[2][0])
-
-        return ', '.join(student_schedule)
-    except (KeyError, ValueError):
-        print(KeyError, ValueError)
-
-
-def get_best_schedule(student_schedule, failed_subject, gpa):
-    """
-        Searching on which subject in the schedule is depended on the failed subjects
-                change the failed subjects with subject which depend on it
-
-        Parameters:
-            student_schedule: list of semester subjects
-            failed_subject: list of failed subjects
-            gpa: int of student GPA
-
-        Return:
-            best_schedule: string of best schedule
-    """
-
-    for i in student_schedule:
-        index = student_schedule.index(i)
-        # if subject is in bylaw
-        if i in bylaw.keys():
-            # check on this subject has 2 or 1 perquisite
-            if len(bylaw[i]) > 1:
-                if bylaw[i][0] in failed_subject:
-                    student_schedule[index] = bylaw[0]
-
-                elif bylaw[i][1] in failed_subject:
-                    student_schedule[index] = bylaw[i][1]
-
-            else:
-                if bylaw[i][0] in failed_subject:
-                    student_schedule[index] = bylaw[i][0]
-
-    best_schedule = get_subjects_priority(student_schedule, int(gpa))
-    return best_schedule
-
-
-def get_failed_subjects(student_grades, year, gpa):
-    """
-        Parameters:
-            student_grades: dictionary of student's results
-            year: string of student's year and his department
-            gpa: string of GPA
-
-        Return:
-            best_schedule: string of best schedule
-    """
-
-    try:
-        # get schedule of his year
-        student_schedule = schedule[year]
-        # get failed subjects
-        failed_subject = student_grades['f']
-        best_schedule = get_best_schedule(student_schedule, failed_subject, gpa)
-        return best_schedule
-    except KeyError:
-        return 'سجل الجدول الطبيعى'
-
-
-def ask_for_one_subject(subject, student_grades):
-    """
-        Parameters:
-            subject: string for subject name
-            student_grades: dictionary of student's results
-
-        Return:
-            string tell student can register this subject or not
-    """
-    subject = map_subject_name(subject, list(bylaw.keys()))
-    try:
-        # get prerequisites of this subject
-        subject_prerequisites = bylaw[subject]
-        if subject_prerequisites[0][0] == 'none':
-            return 'تقدر تسجلها'
-
-    except KeyError:
-        return subject
-
-    for i in subject_prerequisites:
-        try:
-            # check he take and pass these prerequisites
-            failed = student_grades['f']
-
-            if i not in student_grades.values() or i in failed:
-                return f'لاز تاخد {i} الأول'
-
-        except KeyError:
-            continue
-
-    return 'تقدر تسجلها'
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        """ it is the function which execute action_check_subject_name """
+        subject = tracker.get_slot('subject')
+        print(subject)
+        if subject is None:
+            dispatcher.utter_message('بعد أذنك دخل أسم المادة بالأنجليزى')
+            return []
+        
+        tracker.slots.clear()
+        print(tracker.get_slot('subject'))
+        dispatcher.utter_message('الid لو سمحت')
+        return []
 
 
 class ValidateUserDetailsForm(FormValidationAction):
@@ -173,6 +58,7 @@ class ValidateUserDetailsForm(FormValidationAction):
             if subject_name is not None:
                 # if student ask for specific subject
                 message = ask_for_one_subject(subject_name, student_grades)
+                tracker.slots.clear()
                 dispatcher.utter_message(message)
                 return {"id": slot_value}
 
